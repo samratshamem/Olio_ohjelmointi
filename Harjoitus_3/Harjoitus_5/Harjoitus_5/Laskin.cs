3778365ccac929bc9f14b137da0083ca65565d0e
shamem.samrat@gmail.com <shamem.samrat@gmail.com>
2024-06-05 13:35:58 +0300

    kakki tehtävät

---
+++ Harjoitus_3/Harjoitus_5/Harjoitus_5/Laskin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_5
+{
+    static class Laskin
+    {
+        public static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
